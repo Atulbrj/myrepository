5c64e467b9e9af7a4e446a24e3dbb90291083302
admin <admin@DESKTOP-L35N3QV>
2022-11-15 18:13:08 +0530

    Atulcommit

---
+++ org.github/src/test/java/pack1/testclass1.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class testclass1 {
+public static void main(String[] args) {
+	System.out.println("TC01");
+	System.out.println("TC02");
+	System.out.println("TC03");
+	System.out.println("TC04");
+	System.out.println("TC05");
+}
+}
